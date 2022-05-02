ea5bd8994b283a5b7ea2003788f112a1ea0a7ed1
yossibank <yossibank@yahoo.co.jp>
2022-05-02 23:19:01 +0900

    FruitModelテスト追加

--- SwiftCombineTests/Model/FruitModelTests.swift
+++ SwiftCombineTests/Model/FruitModelTests.swift
@@ -8,11 +8,28 @@ final class FruitModelTests: XCTestCase {
         model = Model.CoreData.Fruit(useTestData: true)
     }
 
-    func testFruitModel() throws {
+    func testAddFruitModel() throws {
         model.add(.init(name: "Apple"))
         model.add(.init(name: "Lemon"))
+        model.add(.init(name: "Peach"))
 
         let result = try awaitPublisher(model.fetchAll())
+
+        XCTAssertTrue(result.contains { $0.name == "Apple" })
+        XCTAssertEqual(result.count, 3)
+    }
+
+    func testDeleteFuritModel() throws {
+        model.add(.init(name: "Apple"))
+        model.add(.init(name: "Lemon"))
+        model.add(.init(name: "Peach"))
+
+        let predicate = NSPredicate(format: "%K=%@", "name", "Apple")
+        model.delete(predicate: predicate)
+
+        let result = try awaitPublisher(model.fetchAll())
+
+        XCTAssertFalse(result.contains { $0.name == "Apple" })
         XCTAssertEqual(result.count, 2)
     }
 }
