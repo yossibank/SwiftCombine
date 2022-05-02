c3c4e62050fe3b2aa9c8fc0071b2aa0e19a858da
yossibank <yossibank@yahoo.co.jp>
2022-05-02 14:35:16 +0900

    Mapperテスト作成

---
+++ SwiftCombineTests/Mapper/MapperTests.swift
@@ -0,0 +1,67 @@
+@testable import SwiftCombine
+import XCTest
+
+final class MapperTests: XCTestCase {
+    func testFruitMapper() {
+        let coreDataFruit: Fruit = CoreDataRepository().create()
+        coreDataFruit.name = "Fruit"
+
+        let result = FruitMapper().convert(
+            response: [coreDataFruit]
+        )
+
+        let expect: [FruitEntity] = [
+            .init(name: "Fruit")
+        ]
+
+        XCTAssertEqual(result, expect)
+    }
+
+    func testJokeRandomMapper() {
+        let result = JokeRandomMapper().convert(
+            response: .init(
+                id: "11111",
+                joke: "joke joke",
+                status: 200
+            )
+        )
+
+        let expect: JokeRandomEntity = .init(
+            id: "11111",
+            joke: "joke joke",
+            status: 200
+        )
+
+        XCTAssertEqual(result, expect)
+    }
+
+    func testJokeSlackMapper() {
+        let result = JokeSlackMapper().convert(
+            response: .init(
+                attachments: [
+                    .init(
+                        fallback: "fallback",
+                        footer: "footer",
+                        text: "text"
+                    )
+                ],
+                responseType: "response_type",
+                username: "username"
+            )
+        )
+
+        let expect: JokeSlackEntity = .init(
+            attachments: [
+                .init(
+                    fallback: "fallback",
+                    footer: "footer",
+                    text: "text"
+                )
+            ],
+            responseType: "response_type",
+            username: "username"
+        )
+
+        XCTAssertEqual(result, expect)
+    }
+}
