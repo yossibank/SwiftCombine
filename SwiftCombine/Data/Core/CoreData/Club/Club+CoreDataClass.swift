30d961a43b1212e7f10038f863a2f35c5de14e2c
yossibank <yossibank@yahoo.co.jp>
2022-05-07 19:11:20 +0900

    ClubObjectにconfigure作成

--- SwiftCombine/Data/Core/CoreData/Club/Club+CoreDataClass.swift
+++ SwiftCombine/Data/Core/CoreData/Club/Club+CoreDataClass.swift
@@ -1,4 +1,11 @@
 import CoreData
 
 @objc(Club)
-public class Club: NSManagedObject {}
+public class Club: NSManagedObject {
+    func configure(_ entity: ClubEntity) {
+        name = entity.name
+        money = Int32(entity.money)
+        place = entity.place
+        schedule = entity.schedule
+    }
+}
--- SwiftCombine/Model/Impl/CoreData/ClubModel.swift
+++ SwiftCombine/Model/Impl/CoreData/ClubModel.swift
@@ -18,24 +18,33 @@ extension ModelImpl where R == Repos.Local.ClubCoreData, M == ClubMapper {
 
     func mock() {
         let club1 = repository.object()
-        club1.name = "サッカー部"
-        club1.money = 30000
-        club1.place = "グラウンド"
-        club1.schedule = "火水木"
+        club1.configure(.init(
+            name: "サッカー部",
+            money: 30000,
+            place: "グラウンド",
+            schedule: "火水木",
+            students: []
+        ))
         repository.add(club1)
 
         let club2 = repository.object()
-        club2.name = "野球部"
-        club2.money = 10000
-        club2.place = "公園"
-        club2.schedule = "月金土"
+        club2.configure(.init(
+            name: "野球部",
+            money: 10000,
+            place: "公園",
+            schedule: "月金土",
+            students: []
+        ))
         repository.add(club2)
 
         let club3 = repository.object()
-        club3.name = "体操部"
-        club3.money = 8000
-        club3.place = "講堂"
-        club3.schedule = "土日"
+        club3.configure(.init(
+            name: "体操部",
+            money: 8000,
+            place: "講堂",
+            schedule: "土日",
+            students: []
+        ))
         repository.add(club3)
     }
 }
