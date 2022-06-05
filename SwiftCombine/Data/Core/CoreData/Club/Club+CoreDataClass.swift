2afdff27ded4ac8ffc1ea5f7c0db8ee71e5a33e1
yossibank <yossibank@yahoo.co.jp>
2022-06-05 13:46:45 +0900

    CoreData処理追加

--- SwiftCombine/Data/Core/CoreData/Club/Club+CoreDataClass.swift
+++ SwiftCombine/Data/Core/CoreData/Club/Club+CoreDataClass.swift
@@ -1,4 +1,25 @@
 import CoreData
 
 @objc(Club)
-public class Club: NSManagedObject {}
+public class Club: NSManagedObject {
+    public convenience init(name: String, money: Int, place: String?, schedule: String?) {
+        let context = CoreDataManager.shared.context
+
+        self.init(context: context)
+        self.name = name
+        self.money = Int32(money)
+        self.place = place
+        self.schedule = schedule
+    }
+
+    static func create(name: String, money: Int, place: String?, schedule: String?) -> Club {
+        if let club = CoreDataHolder.clubs.filter({ $0.name == name }).first {
+            club.money = Int32(money)
+            club.place = place
+            club.schedule = schedule
+            return club
+        } else {
+            return .init(name: name, money: money, place: place, schedule: schedule)
+        }
+    }
+}
